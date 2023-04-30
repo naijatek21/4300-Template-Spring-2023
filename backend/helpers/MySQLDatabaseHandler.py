28e93392374ce2db662e4c0ed50a5baa065b50a3
Michelle Prior <67717433+michpdev@users.noreply.github.com>
2023-04-30 13:04:31 -0400

    Update MySQLDatabaseHandler.py

--- backend/helpers/MySQLDatabaseHandler.py
+++ backend/helpers/MySQLDatabaseHandler.py
@@ -34,7 +34,7 @@ class MySQLDatabaseHandler(object):
 
     def query_selector(self,query):
         conn = self.lease_connection()
-        conn.execute(f"USE {self.MYSQL_DATABASE}")
+        #conn.execute(f"USE {self.MYSQL_DATABASE}")
         data = conn.execute(query)
         return data
 
